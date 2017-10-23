06da7f899a735f3214dc1aedde8c53d470c1d5ec
skreynolds <shane.k.reynolds@gmail.com>
2017-10-23 21:59:31 +0930

    Implemented Forward Kinematics - need to test first

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -25,23 +25,72 @@ def handle_calculate_IK(req):
         print "No valid poses received"
         return -1
     else:
-		
         ### Your FK code here
         # Create symbols
-	#
-	#   
-	# Create Modified DH parameters
-	#
-	#            
-	# Define Modified DH Transformation matrix
-	#
-	#
-	# Create individual transformation matrices
-	#
-	#
-	# Extract rotation matrices from the transformation matrices
-	#
-	#
+        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+        # Create Modified DH parameters
+        s = {alpha0:     0,    a0:    0,    d1: 0.68,
+             alpha1: -pi/2,    a1: 0.42,    d2:    0,    q2: (q2 - pi/2),
+             alpha2:     0,    a2: 1.25,    d3:    0,
+             alpha3: -pi/2,    a3: 0.54,    d4:  1.5,
+             alpha4:  pi/2,    a4:    0,    d5:    0,
+             alpha5: -pi/2,    a5:    0,    d6:    0,
+             alpha6:     0,    a6:    0,    d7: 0.11,    q7: 0}
+
+        # Define Modified DH Transformation matrix
+        '''T0_1 = Matrix([[            cos(q1),            -sin(q1),            0,              a0],
+                       [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+                       [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+                       [                  0,                   0,            0,               1]])
+
+        T1_2 = Matrix([[            cos(q2),            -sin(q2),            0,              a1],
+                       [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+                       [sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+                       [                  0,                   0,            0,               1]])
+
+        T2_3 = Matrix([[            cos(q3),            -sin(q3),            0,              a2],
+                       [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+                       [sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+                       [                  0,                   0,            0,               1]])
+
+        T2_3 = Matrix([[            cos(q4),            -sin(q4),            0,              a3],
+                       [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d3],
+                       [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d3],
+                       [                  0,                   0,            0,               1]])
+
+        T2_3 = Matrix([[            cos(q4),            -sin(q4),            0,              a3],
+                       [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d3],
+                       [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d3],
+                       [                  0,                   0,            0,               1]])'''
+        T0_1 = transMat(q1, alpha0, d1, a0)
+        T1_2 = transMat(q2, alpha1, d2, a1)
+        T2_3 = transMat(q3, alpha2, d3, a2)
+        T3_4 = transMat(q4, alpha3, d4, a3)
+        T4_5 = transMat(q5, alpha4, d5, a4)
+        T5_6 = transMat(q5, alpha5, d6, a5)
+        T6_7 = transMat(q7, alpha6, d7, a6)
+
+        # Create individual transformation matrices
+        T0_1 = T0_1.subs(s)
+        T1_2 = T0_1.subs(s)
+        T2_3 = T0_1.subs(s)
+        T3_4 = T0_1.subs(s)
+        T4_5 = T0_1.subs(s)
+        T5_6 = T0_1.subs(s)
+        T6_7 = T0_1.subs(s)
+
+        # Extract rotation matrices from the transformation matrices
+        R0_1 = T0_1[0:3,0:3]
+        R1_2 = T1_2[0:3,0:3]
+        R2_3 = T2_3[0:3,0:3]
+        R3_4 = T3_4[0:3,0:3]
+        R4_5 = T4_5[0:3,0:3]
+        R5_6 = T5_6[0:3,0:3]
+        R6_7 = T6_7[0:3,0:3]
         ###
 
         # Initialize service response
@@ -60,8 +109,8 @@ def handle_calculate_IK(req):
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
-     
-            ### Your IK code here 
+
+            ### Your IK code here
 	    # Compensate for rotation discrepancy between DH parameters and Gazebo
 	    #
 	    #
@@ -69,7 +118,7 @@ def handle_calculate_IK(req):
 	    #
 	    #
             ###
-		
+
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
 	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
@@ -77,6 +126,14 @@ def handle_calculate_IK(req):
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
+
+
+def transMat(q, alpha, d, a):
+    T = Matrix([[           cos(q),           -sin(q),           0,             a],
+                [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                [sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+                [                 0,                0,           0,             1]])
+    return T
 
 
 def IK_server():

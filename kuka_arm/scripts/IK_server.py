d07adc219fdd2b5ddb694f10ee8bb44170945744
skreynolds <shane.k.reynolds@gmail.com>
2017-10-23 23:31:11 +0930

    Update to the FK portion of the IK service

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -33,39 +33,15 @@ def handle_calculate_IK(req):
         alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 
         # Create Modified DH parameters
-        s = {alpha0:     0,    a0:    0,    d1: 0.68,
-             alpha1: -pi/2,    a1: 0.42,    d2:    0,    q2: (q2 - pi/2),
-             alpha2:     0,    a2: 1.25,    d3:    0,
-             alpha3: -pi/2,    a3: 0.54,    d4:  1.5,
-             alpha4:  pi/2,    a4:    0,    d5:    0,
-             alpha5: -pi/2,    a5:    0,    d6:    0,
-             alpha6:     0,    a6:    0,    d7: 0.11,    q7: 0}
+        s = {alpha0:     0,    a0:      0,    d1: 0.68,
+             alpha1: -pi/2,    a1:   0.42,    d2:    0,    q2: (q2 - pi/2),
+             alpha2:     0,    a2:   1.25,    d3:    0,
+             alpha3: -pi/2,    a3: -0.054,    d4:  1.5,
+             alpha4:  pi/2,    a4:      0,    d5:    0,
+             alpha5: -pi/2,    a5:      0,    d6:    0,
+             alpha6:     0,    a6:      0,    d7: 0.11,    q7: 0}
 
         # Define Modified DH Transformation matrix
-        '''T0_1 = Matrix([[            cos(q1),            -sin(q1),            0,              a0],
-                       [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
-                       [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
-                       [                  0,                   0,            0,               1]])
-
-        T1_2 = Matrix([[            cos(q2),            -sin(q2),            0,              a1],
-                       [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
-                       [sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
-                       [                  0,                   0,            0,               1]])
-
-        T2_3 = Matrix([[            cos(q3),            -sin(q3),            0,              a2],
-                       [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
-                       [sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
-                       [                  0,                   0,            0,               1]])
-
-        T2_3 = Matrix([[            cos(q4),            -sin(q4),            0,              a3],
-                       [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d3],
-                       [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d3],
-                       [                  0,                   0,            0,               1]])
-
-        T2_3 = Matrix([[            cos(q4),            -sin(q4),            0,              a3],
-                       [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d3],
-                       [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d3],
-                       [                  0,                   0,            0,               1]])'''
         T0_1 = transMat(q1, alpha0, d1, a0)
         T1_2 = transMat(q2, alpha1, d2, a1)
         T2_3 = transMat(q3, alpha2, d3, a2)
@@ -74,14 +50,21 @@ def handle_calculate_IK(req):
         T5_6 = transMat(q5, alpha5, d6, a5)
         T6_7 = transMat(q7, alpha6, d7, a6)
 
-        # Create individual transformation matrices
         T0_1 = T0_1.subs(s)
-        T1_2 = T0_1.subs(s)
-        T2_3 = T0_1.subs(s)
-        T3_4 = T0_1.subs(s)
-        T4_5 = T0_1.subs(s)
-        T5_6 = T0_1.subs(s)
-        T6_7 = T0_1.subs(s)
+        T1_2 = T1_2.subs(s)
+        T2_3 = T2_3.subs(s)
+        T3_4 = T3_4.subs(s)
+        T4_5 = T4_5.subs(s)
+        T5_6 = T5_6.subs(s)
+        T6_7 = T6_7.subs(s)
+
+        # Create individual transformation matrices
+        T0_2 = T0_1*T1_2
+        T0_3 = T0_2*T2_3
+        T0_4 = T0_3*T3_4
+        T0_5 = T0_4*T4_5
+        T0_6 = T0_5*T5_6
+        T0_7 = T0_6*T6_7 # This is the full Homogeneous transform to the gripper
 
         # Extract rotation matrices from the transformation matrices
         R0_1 = T0_1[0:3,0:3]
